dd6f389ccad3e4e2e4fc403821ead892a9b9d02b
Songxd <78064887+songxdr3@users.noreply.github.com>
2025-09-22 21:09:03 +0800

    Add files via upload

---
+++ UpDn_DSI/utils.py
@@ -0,0 +1,117 @@
+import os
+import json
+import errno
+import numpy as np
+
+def assert_eq(real, expected):
+    assert real == expected, '%s (true) vs %s (expected)' % (real, expected)
+
+def assert_array_eq(real, expected):
+    assert (np.abs(real-expected) < EPS).all(), \
+        '%s (true) vs %s (expected)' % (real, expected)
+
+class Tracker:
+    """ Keep track of results over time, while having access to
+        monitors to display information about them.
+    """
+    def __init__(self):
+        self.data = {}
+
+    def track(self, name, *monitors):
+        """ Track a set of results with given monitors under some name (e.g. 'val_acc').
+            When appending to the returned list storage, use the monitors
+            to retrieve useful information.
+        """
+        l = Tracker.ListStorage(monitors)
+        self.data.setdefault(name, []).append(l)
+        return l
+
+    def to_dict(self):
+        # turn list storages into regular lists
+        return {k: list(map(list, v)) for k, v in self.data.items()}
+
+
+    class ListStorage:
+        """ Storage of data points that updates the given monitors """
+        def __init__(self, monitors=[]):
+            self.data = []
+            self.monitors = monitors
+            for monitor in self.monitors:
+                setattr(self, monitor.name, monitor)
+
+        def append(self, item):
+            for monitor in self.monitors:
+                monitor.update(item)
+            self.data.append(item)
+
+        def __iter__(self):
+            return iter(self.data)
+
+    class MeanMonitor:
+        """ Take the mean over the given values """
+        name = 'mean'
+
+        def __init__(self):
+            self.n = 0
+            self.total = 0
+
+        def update(self, value):
+            self.total += value
+            self.n += 1
+
+        @property
+        def value(self):
+            return self.total / self.n
+
+    class MovingMeanMonitor:
+        """ Take an exponentially moving mean over the given values """
+        name = 'mean'
+
+        def __init__(self, momentum=0.9):
+            self.momentum = momentum
+            self.first = True
+            self.value = 0
+
+        def update(self, value):
+            if self.first:
+                self.value = value
+                self.first = False
+            else:
+                m = self.momentum
+                self.value = m * self.value + (1 - m) * value
+
+class Logger(object):
+    def __init__(self, output_name):
+        dirname = os.path.dirname(output_name)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+        self.log_file = open(output_name, 'w')
+        self.infos = {}
+
+    def append(self, key, val):
+        vals = self.infos.setdefault(key, [])
+        vals.append(val)
+
+    def log(self, extra_msg=''):
+        msgs = [extra_msg]
+        for key, vals in self.infos.iteritems():
+            msgs.append('%s %.6f' % (key, np.mean(vals)))
+        msg = '\n'.join(msgs)
+        self.log_file.write(msg + '\n')
+        self.log_file.flush()
+        self.infos = {}
+        return msg
+
+    def write(self, msg):
+        self.log_file.write(msg + '\n')
+        self.log_file.flush()
+        print(msg)
+
+def create_dir(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
